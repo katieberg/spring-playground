eaa209d38d4a083a768cffe8bf1fd02c39e2cce8
unknown <kberg1@email.arizona.edu>
2019-08-13 13:28:32 -0700

    test working

--- src/main/java/com/galvanize/playground/PlaygroundApplication.java
+++ src/main/java/com/galvanize/playground/PlaygroundApplication.java
@@ -2,12 +2,23 @@ package com.galvanize.playground;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class PlaygroundApplication {
-
+	@RestController
+	@RequestMapping("/math")
+	public class MyController {
+		@GetMapping("/pi")
+		public String myMethod() {
+			return "3.141592653589793";
+		}
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(PlaygroundApplication.class, args);
 	}
 
 }
+
--- src/test/java/com/galvanize/playground/PlaygroundApplicationTests.java
+++ src/test/java/com/galvanize/playground/PlaygroundApplicationTests.java
@@ -1,16 +1,31 @@
 package com.galvanize.playground;
 
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(PlaygroundApplication.class)
 public class PlaygroundApplicationTests {
+	@Autowired
+	private MockMvc mvc;
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		this.mvc.perform(get("/math/pi").accept(MediaType.TEXT_PLAIN))
+				.andExpect(status().isOk())
+				.andExpect(content().string("3.141592653589793"));
 	}
 
 }
