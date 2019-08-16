cfdcb3658da5a461cb59b47f86515fe1bb0e391a
unknown <kberg1@email.arizona.edu>
2019-08-16 11:54:01 -0700

    setting up MySQL database

---
+++ src/main/java/com/galvanize/playground/LessonRepository.java
@@ -0,0 +1,8 @@
+package com.galvanize.playground;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface LessonRepository extends CrudRepository<Lesson, Long> {
+
+
+}
--- src/test/java/com/galvanize/playground/PlaygroundApplicationTests.java
+++ src/test/java/com/galvanize/playground/PlaygroundApplicationTests.java
@@ -29,22 +29,22 @@ public class PlaygroundApplicationTests {
 				.andExpect(content().string("3.141592653589793"));
 	}
 
-//	@Test
-//	public void testOperation() throws Exception {
-//		this.mvc.perform(get("/math/calculate?operation=add&x=4&y=6"))
-//				.andExpect(status().isOk())
-//				.andExpect(content().string("4 + 6 = 10"));
-//		this.mvc.perform(get("/math/calculate?operation=multiply&x=4&y=6"))
-//				.andExpect(status().isOk())
-//				.andExpect(content().string("4 * 6 = 24"));
-//		this.mvc.perform(get("/math/calculate?operation=subtract&x=4&y=6"))
-//				.andExpect(status().isOk())
-//				.andExpect(content().string("4 - 6 = -2"));
-//		this.mvc.perform(get("/math/calculate?operation=divide&x=30&y=5"))
-//				.andExpect(status().isOk())
-//				.andExpect(content().string("30 / 5 = 6"));
-//		this.mvc.perform(get("/math/calculate?x=30&y=5"))
-//				.andExpect(status().isOk())
-//				.andExpect(content().string("30 + 5 = 35"));
-//	}
+	@Test
+	public void testOperation() throws Exception {
+		this.mvc.perform(get("/math/calculate?operation=add&x=4&y=6"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("4 + 6 = 10"));
+		this.mvc.perform(get("/math/calculate?operation=multiply&x=4&y=6"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("4 * 6 = 24"));
+		this.mvc.perform(get("/math/calculate?operation=subtract&x=4&y=6"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("4 - 6 = -2"));
+		this.mvc.perform(get("/math/calculate?operation=divide&x=30&y=5"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("30 / 5 = 6"));
+		this.mvc.perform(get("/math/calculate?x=30&y=5"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("30 + 5 = 35"));
+	}
 }
